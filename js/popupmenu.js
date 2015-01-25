990acd9dc4e93a68846253a0b931b447a03c854e
MircMM <mircmm@gmail.com>
2015-01-25 08:28:01 +0100

    WORKING version before refactoring
    code clean up

--- js/popupmenu.js
+++ js/popupmenu.js
@@ -1,13 +1,41 @@
+// ----- init -----------------------------------------------------------------
+  var cpInit = {
+    color: "hsl(180, 50%, 50%)",
+    previewformat: "hsl",
+    rendervalues: true,
+    slidersplusminus: true,
+    flat: true,
+    sliders: true,
+    swatches: false,
+    hsvpanel: false,
+    order: {
+      preview: 1,
+      hslL: 6
+    }
+  };
+  var cpMaxLight = $.extend( {}, cpInit, { color: "hsl(180, 50%, 90%)" });
+  var cpHueSat   = $.extend( {}, cpInit, { order: { preview: 1, hslH: 5, hslS: 6 } });
+  var cpMinLight = $.extend( {}, cpInit, { color: "hsl(180, 50%, 10%)" });
+
+
+
+// ----- main -----------------------------------------------------------------
 $( document ).ready(function() {
   'use strict';
-
 //  alert("dela");
 
+  // ----- make my menu draggable inside browser window -----------------------
   $( "#mm-menu-main" ).draggable({
     handle: "#mm-menu-drag",
     containment: "window"
   });
 
+  // ----- init color sliders in colors menu ----------------------------------
+  $(" #cp-max-light ").ColorPickerSliders( cpMaxLight );
+  $(" #cp-hue-sat ").ColorPickerSliders( cpHueSat );
+  $(" #cp-min-light ").ColorPickerSliders( cpMinLight );
+
+  // ----- event handlers -----------------------------------------------------
   $( ".mm-menu-top" ).on( "click", function() {
     $( ".mm-menu-top+div" ).hide();
     $( this ).next().show();
@@ -18,56 +46,6 @@ $( document ).ready(function() {
   });
 
 
-
-  $("#cp-max-light").ColorPickerSliders({
-    color: "hsl(180, 50%, 100%)",
-    previewformat: 'hsl-rgb',
-    rendervalues: true,
-    slidersplusminus: true,
-    flat: true,
-    sliders: true,
-    swatches: false,
-    hsvpanel: false,
-    order: {
-      preview: 1,
-      hslL: 6
-    }
-  });
-
-
-  $("#cp-min-light").ColorPickerSliders({
-    color: "hsl(180, 50%, 0%)",
-    previewformat: 'hsl-rgb',
-    rendervalues: true,
-    slidersplusminus: true,
-    flat: true,
-    sliders: true,
-    swatches: false,
-    hsvpanel: false,
-    order: {
-      preview: 1,
-      hslL: 6
-    }
-  });
-
-  $("#cp-hue-sat").ColorPickerSliders({
-    color: "hsl(180, 50%, 50%)",
-    previewformat: 'hsl-rgb',
-    rendervalues: true,
-    slidersplusminus: true,
-    flat: true,
-    sliders: true,
-    swatches: false,
-    hsvpanel: false,
-    order: {
-      preview: 1,
-      hslH: 4,
-      hslS: 5
-    }
-  });
-
-
-}); // ----- end of document ready -----
-
+}); // ----- end of document ready --------------------------------------------
 
 
