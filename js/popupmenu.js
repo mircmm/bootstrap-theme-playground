f537a61f3d92eb67229545e7bd4e587e662ac769
MircMM <mircmm@gmail.com>
2015-01-26 22:53:49 +0100

    render Color menija dela OK
    nastavljen call back za update barv v tabeli

--- js/popupmenu.js
+++ js/popupmenu.js
@@ -18,35 +18,39 @@
 
 //    return this.each(function() {
 
-      var settings,
-          triggerelement = $(this), // body
-          popover_container,
-          elements,
-          connectedinput = false,
-          // default sliders options
-          mm_color_light = [100, 96, 92, 88, 80, 60, 50, 32, 26, 20, 12, 0],
-          cpInit = {
-            color: "hsl(180, 50%, 50%)",
-            previewformat: "hsl",
-            rendervalues: true,
-            slidersplusminus: true,
-            flat: true,
-            sliders: true,
-            swatches: false,
-            hsvpanel: false,
-            order: {
-              preview: 1,
-              hslL: 6
-            }
-          },
-          cpMaxLight = $.extend( {}, cpInit,
-            { color: "hsl(180, 50%, 90%)" }),
-          cpHueSat   = $.extend( {}, cpInit,
-            { order: { preview: 1, hslH: 5, hslS: 6 } }),
-          cpMinLight = $.extend( {}, cpInit,
-            { color: "hsl(180, 50%, 10%)" }),
-          title = "Bootstrap Theme Playground",
-          lastVar;
+      var
+        title = "Bootstrap Theme Playground",
+        settings,
+        triggerelement = $(this), // body
+        popover_container,
+        elements,
+        connectedinput = false,
+        mm_color_light = [100, 96, 92, 88, 80, 60, 50, 32, 26, 20, 12, 0],
+        // default sliders options
+        cpDefault = {
+          previewformat: "hsl",
+          rendervalues: true,
+          slidersplusminus: true,
+          flat: true,
+          sliders: true,
+          swatches: false,
+          hsvpanel: false,
+          onchange: _onColorChange
+        },
+        // specific slider options
+        cpMaxLight = $.extend( {}, cpDefault, {
+          color: "hsl(180, 50%, 90%)",
+          order: { hslL: 6 }
+        }),
+        cpHueSat   = $.extend( {}, cpDefault, {
+          color: "hsl(180, 50%, 50%)",
+          order: { hslH: 4, hslS: 5 }
+        }),
+        cpMinLight = $.extend( {}, cpDefault, {
+          color: "hsl(180, 50%, 10%)",
+          order: { hslL: 6 }
+        }),
+        lastVar;
       // end of variables
 
       init();
@@ -69,34 +73,27 @@
       } // init
 
       function _showPopover() {
-//        if (popover_container instanceof jQuery) {
-//          return;
-//        }
-
         popover_container = $('<div id="mm-menu-main"></div>').appendTo('body');
         popover_container.html( _getControllerHtml() );
-
         // ----- make menu draggable inside browser window --------------------
         $( "#mm-menu-main" ).draggable({
           handle: "#mm-menu-drag",
           containment: "window"
         });
-
         // ----- init color sliders in colors menu ----------------------------
-        $(" #mm-color-light-100" ).ColorPickerSliders( cpMaxLight );
-        $(" #mm-color-light-50" ).ColorPickerSliders( cpHueSat );
-        $(" #mm-color-light-0" ).ColorPickerSliders( cpMinLight );
+        $(" #mm-color-light-100" ).removeClass( "mm-preview" ).addClass( "mm-preview-bm1" ).ColorPickerSliders( cpMaxLight );
+        $(" #mm-color-light-50" ).removeClass( "mm-preview" ).addClass( "mm-preview-bm2" ).ColorPickerSliders( cpHueSat );
+        $(" #mm-color-light-0" ).removeClass( "mm-preview" ).addClass( "mm-preview-bm1" ).ColorPickerSliders( cpMinLight );
       } // showPopover
 
       function _getControllerHtml() {
         var popup_menu_html = '';
-
-        popup_menu_html += '<ul>' ;
         // dragable top
+        popup_menu_html += '<ul>' ;
         popup_menu_html +=
           '<li><span class="mm-menu-title mm-menu-top" id="mm-menu-drag">' + title + '</span></li>';
         popup_menu_html += '</ul>' ;
-
+        // menus
         popup_menu_html += '<ul>' ;
         // file menu
         popup_menu_html +=
@@ -133,22 +130,27 @@
               '</ul>' +
             '</div>' +
           '</li>';
+        // end of menus
         popup_menu_html += '</ul>' ;
-
         return popup_menu_html;
       } // _getControllerHtml
 
       function _getColorsTable () {
         var colors_html = '';
         // use array mm_color_light
-
         for (var c in mm_color_light) {
           colors_html +=
             '<div class="mm-preview" id="mm-color-light-' + mm_color_light[c] + '">' + mm_color_light[c] + '&#37;</div>';
         }
-
         return colors_html;
-      }
+      } // _getColorsTable
+
+      function _onColorChange(container, color) {
+/* tukaj naredi update background barv za vse % */
+        var a = container.parent();
+        var b = color;
+        var c = null;
+      } // _onColorChange
 
       function _bindEvents() {
         $( ".mm-menu-top" ).on( "click", function() {
@@ -176,8 +178,8 @@ $( document ).ready(function() {
 //  $( "body" ).mmTest();
 
 
-  $( "body" ).PopupMenu();
-//  PopupMenu();
+//  $( "body" ).PopupMenu();
+  $().PopupMenu();
 
 
 }); // ----- end of document ready --------------------------------------------
