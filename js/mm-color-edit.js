31fbaff5678bfa54c215790b6f89856f32c06dc8
MircMM <mircmm@gmail.com>
2015-01-29 23:46:39 +0100

    mm-color-edit priblizno dela
    (ne vem zakaj puscice se vedno migajo ?)

--- js/mm-color-edit.js
+++ js/mm-color-edit.js
@@ -1,17 +1,42 @@
-function mmColorEdit( ev ) {
-
-  alert( ev );
-  var konec = null;
-}
 
 $( document ).ready( function() {
 
-  $( '#id-button' ).on( 'click', function(ev) {
-    event.preventDefault();
-    mmColorEdit( ev );
-  })
+  var cpDefault = {
+    previewformat: 'hsl-rgb',
+    previewontriggerelement: true,
+    rendervalues: true,
+    slidersplusminus: true,
+//    size: 'lg',
+    flat: true,
+    sliders: true,
+    swatches: false,
+    hsvpanel: false,
+    order: {
+        hslL: 4
+      }
+  };
+
+  // var bc = $( '#mm-color-edit-1 .mm-color-view' ).css('background-color');
+  // $( '#mm-color-edit-1 .mm-color-view' ).ColorPickerSliders( $.extend( {color: bc}, cpDefault ) );
+
+
+
+  $( '.mm-pointer' ).on( 'click', function(ev) {
+    ev.preventDefault();
+    var ptr = $(this);
+
+    if ( ptr.prev().hasClass('mm-color-view') ) {
+      var bc = ptr.prev().css('background-color');
+      ptr.prev().ColorPickerSliders( $.extend( {color: bc}, cpDefault ) );
+      ptr.attr('slider', true);
+    } else {
+      ptr.prev().remove();
+      ptr.removeAttr('slider');
+    };
+
+
+  });
 
   var konec = null;
 });
 
-// var $jQueryObject = $($.parseHTML(myString));
