7f40b6e082028419f18e2835c03f93cd8b3128d3
MircMM <mircmm@gmail.com>
2015-02-10 20:54:40 +0100

    gfonts priblizno dela za izbiro fonta iz googla

--- js/gfonts.js
+++ js/gfonts.js
@@ -6,42 +6,73 @@
 
   $.fn.gfonts = function() {
 
+// ----- init -----
+
     var googleAPI = 'AIzaSyDyJtbKog963UG494tay5ydfU-AWhVbZUg';
     var googleFontsURL = 'https://www.googleapis.com/webfonts/v1/webfonts?key='+googleAPI;
-    // https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDyJtbKog963UG494tay5ydfU-AWhVbZUg
-    // var allFonts = $.get( googleFontsURL ).responseJSON.items;
-
     var allFonts;
+    var firstItem = 0;
+    var countOfItems = 10;
 
     var exampleText = 'abc&#269;s&#353;z&#382; ABC&#268;S&#352;Z&#381; 1234567890';
     $( '#mm-display-example' ).html( exampleText );
     exampleText = $( '#mm-display-example' ).html();
     $( '#mm-enter-example' ).val( exampleText );
-    
 
-    function generateSelectFont( first, count, filter ) {
-      // valid filters: 'sans-serif', 'serif', 'display', 'handwriting', 'monospace'
+    var categoryFilters = [ 'all', 'sans-serif', 'serif', 'display', 'handwriting', 'monospace' ];
+    var selectedCategoryFilter = 0;
+    var element = $( '#mm-choose-filter' );
+    for ( var i = 0; i < categoryFilters.length; i++ ) {
+      var opt = $( '<span class="mm-filter" mm-filter-index="' + i + '">' + categoryFilters[i] + '</span>' );
+      element.append( opt ).append( '&nbsp;&nbsp;' );
+    }
+    $( '#mm-display-filter' ).text( categoryFilters[selectedCategoryFilter] );
 
+    $.getJSON( googleFontsURL, function(result) {
+      allFonts = result;
+      generateSelectFont( firstItem, countOfItems  );
+    });
+
+// ----- functions -----
+
+    function generateSelectFont( first, count ) {
       var element = $( '#mm-choose-font' );
+      element.empty();
       for ( var i = first, c = 0; i <= allFonts.items.length && c < count; i++ ) {
         var item = allFonts.items[i];
-        if ( filter && item.category === filter ) { // popravi na filter
+        if ( selectedCategoryFilter === 0 || categoryFilters[selectedCategoryFilter] === item.category ) {
           var opt = $( '<span class="mm-family" mm-font-index="' + i + '">' + item.family + '</span> <span class="mm-example">' + exampleText + '</span><br>' );
           element.append( opt );
           c++;
         }
       }
     }
-    
-    $( '#mm-load-json' ).on( 'click', function(ev) {
+
+
+// ----- events -----
+
+    $( '#mm-choose-filter' ).on( 'click', '.mm-filter', function(ev) {
       ev.preventDefault();
-//      $.getJSON( '/all-google-fonts.js', function(result) {
-      $.getJSON( googleFontsURL, function(result) {
-        allFonts = result;
-        generateSelectFont( 0, 5, 'sans-serif' );
-      });
+      var clicked = $(this);
+      selectedCategoryFilter = parseInt( clicked.attr( 'mm-filter-index' ) );
+      $( '#mm-display-filter' ).text( categoryFilters[selectedCategoryFilter] );
+      firstItem = 0;
+      generateSelectFont( firstItem, countOfItems  );
     });
-    
+
+    $( '#mm-prev-10' ).on( 'click', function(ev) {
+      ev.preventDefault();
+      firstItem -= 10;
+      if ( firstItem < 0 ) { firstItem = 0; };
+      generateSelectFont( firstItem, countOfItems  );
+    });
+
+    $( '#mm-next-10' ).on( 'click', function(ev) {
+      ev.preventDefault();
+      firstItem += 10; // tole ni prav, preskocit bi moral glede na selected category
+      generateSelectFont( firstItem, countOfItems );
+    });
+
     $( '#mm-choose-font' ).on( 'click', '.mm-family', function(ev) {
       ev.preventDefault();
       var clicked = $(this);
@@ -52,15 +83,16 @@
       });
       var familyAndFallback = '"' + item.family + '",' + item.category;
       $( '#mm-display-example' ).css( 'font-family', familyAndFallback );
+      familyAndFallback = ' (' + i + '/' + allFonts.items.length + ') ' + familyAndFallback;
       $( '#mm-display-font' ).text( familyAndFallback );
     });
-    
+
     $( '#mm-update-example' ).on( 'click', function(ev) {
       ev.preventDefault();
       var txt = $( '#mm-enter-example' ).val();
       $( '#mm-display-example' ).text( txt );
     });
-  
+
   }; // gfonts
 
 })(jQuery);
